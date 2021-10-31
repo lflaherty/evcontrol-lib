3fcde8e05f65b50ad68ec954780526c09cdbdc7f
Liam Flaherty <liamjfla@gmail.com>
2021-10-31 18:32:28 +1000

    implement transforms

--- src/transforms.c
+++ src/transforms.c
@@ -7,17 +7,30 @@
 
 #include "transforms.h"
 
-void parkTransform(idq_T* idq, const iabc_T* iabc, const real_T theta_e)
+#include <math.h>
+#include "constants.h"
+
+void parkTransform(idq_T* idq, const iabc_T* iabc, const float theta_e)
 {
-    idq->id = 0;
-    idq->iq = 0;
-    // TODO implement
+    idq->id = sinf(theta_e)*iabc->ia 
+                + sinf(theta_e - TWO_PI_3)*iabc->ib 
+                + sinf(theta_e + TWO_PI_3)*iabc->ic;
+    idq->id *= 0.666667F;
+
+    idq->iq = cosf(theta_e)*iabc->ia 
+                + cosf(theta_e - TWO_PI_3)*iabc->ib 
+                + cosf(theta_e + TWO_PI_3)*iabc->ic;
+    idq->iq *= 0.666667F;
 }
 
-void inverseParkTransform(Vabc_T* Vabc, const Vdq_T* vdq, const real_T theta_e)
+void inverseParkTransform(Vabc_T* Vabc, const Vdq_T* vdq, const float theta_e)
 {
-    Vabc->va = 0;
-    Vabc->vb = 0;
-    Vabc->vc = 0;
-    // TODO implement
+    Vabc->va = sin(theta_e)*vdq->vd 
+                + cos(theta_e)*vdq->vq;
+
+    Vabc->vb = sin(theta_e - TWO_PI_3)*vdq->vd
+                + cos(theta_e - TWO_PI_3)*vdq->vq;
+    
+    Vabc->vc = sin(theta_e + TWO_PI_3)*vdq->vd
+                + cos(theta_e + TWO_PI_3)*vdq->vq;
 }
