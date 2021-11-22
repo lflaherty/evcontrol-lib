425c79fe4f45bec51dbc622200d73ff744e1bdc0
Liam Flaherty <liamjfla@gmail.com>
2021-11-22 21:29:49 +1000

    alternate implementation

--- mImpl/inverseParkTransform.m
+++ mImpl/inverseParkTransform.m
@@ -1,9 +1,19 @@
 function [Vabc] = inverseParkTransform(Vdq, theta_e)
-    Vdq0 = [Vdq(1); Vdq(2); 0];
+%     Vdq0 = [Vdq(1); Vdq(2); 0];
+%     phase = 2*pi/3;
+%     M = [sin(theta_e),         cos(theta_e),         1; ...
+%          sin(theta_e - phase), cos(theta_e - phase), 1; ...
+%          sin(theta_e + phase), cos(theta_e + phase), 1];
+%     Vabc = M*Vdq0;
+
+    vd = Vdq(1);
+    vq = Vdq(2);
+    
     phase = 2*pi/3;
-    M = [sin(theta_e),         cos(theta_e),         1; ...
-         sin(theta_e - phase), cos(theta_e - phase), 1; ...
-         sin(theta_e + phase), cos(theta_e + phase), 1];
-    Vabc = M*Vdq0;
+    va = sin(theta_e)*vd       + cos(theta_e)*vq;
+    vb = sin(theta_e-phase)*vd + cos(theta_e-phase)*vq;
+    vc = sin(theta_e+phase)*vd + cos(theta_e+phase)*vq;
+    
+    Vabc = [va; vb; vc];
 end
 
--- mImpl/parkTransform.m
+++ mImpl/parkTransform.m
@@ -1,12 +1,25 @@
 function [idq] = parkTransform(iabc,theta)
+%     phase = 2*pi/3;
+%     half = 1/2;
+%     M = [sin(theta), sin(theta - phase), sin(theta + phase); ...
+%          cos(theta), cos(theta - phase), cos(theta + phase); ...
+%          half,       half,               half];
+%     idq0 = (2/3)*M*iabc;
+%     id = idq0(1);
+%     iq = idq0(2);
+%     idq = [id; iq];
+
+    ia = iabc(1);
+    ib = iabc(2);
+    ic = iabc(3);
+
     phase = 2*pi/3;
-    half = 1/2;
-    M = [sin(theta), sin(theta - phase), sin(theta + phase); ...
-         cos(theta), cos(theta - phase), cos(theta + phase); ...
-         half,       half,               half];
-    idq0 = (2/3)*M*iabc;
-    id = idq0(1);
-    iq = idq0(2);
+    id = sin(theta)*ia + sin(theta-phase)*ib + sin(theta+phase)*ic;
+    id = (2/3)*id;
+    
+    iq = cos(theta)*ia + cos(theta-phase)*ib + cos(theta+phase)*ic;
+    iq = (2/3)*iq;
+    
     idq = [id; iq];
 end
 
