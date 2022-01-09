28813590800baa1c30d764a57b2846f6728b0448
Liam Flaherty <liamjfla@gmail.com>
2022-01-09 21:04:36 +1000

    update gear ratio

--- parameters/DrivetrainSim_Data_Emrax268.m
+++ parameters/DrivetrainSim_Data_Emrax268.m
@@ -49,5 +49,5 @@ AL    = 0.9;    % Max vehicle cross section area [m^2]
 Cd    = 0.4;    % Air drag coefficient           [N*s^2/kg*m]
 cr1   = 0.1;    % Rolling coefficient            
 cr2   = 0.2;    % Rolling coefficient            
-i_t   = 2.3;    % Gear reduction ratio           
+i_t   = 3.9;    % Gear reduction ratio           
 Rw    = 0.3;    % Wheel radius                   [m]
