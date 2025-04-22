a44d44bb00485f9fad5bb87d325c28ebeb302dc0
Liam Flaherty <liamjfla@gmail.com>
2025-04-22 14:29:15 +1000

    update parameters to new motor datasheet

--- sim/parameters/motor_emrax268.m
+++ sim/parameters/motor_emrax268.m
@@ -1,23 +1,27 @@
 %% Parameters for Emrax 268 based powertrain
+% Referencing EMRAX 268 Medium Voltage combined cooling (CC).
+% https://emrax.com/wp-content/uploads/2025/03/EMRAX_268_datasheet_v1.6.pdf
+
 %% Machine Parameters
-Pmax = 230e3;      % Maximum power                   [W]
-Tmax = 500;        % Maximum torque                  [N*m]
-Imax = 565;        % Maximum current                 [A]
-Ld   = 292e-6;     % Stator d-axis inductance        [H]
-Lq   = 272e-6;     % Stator q-axis inductance        [H]
-L0   = 0.00012184; % Stator zero-sequence inductance [H]
-Rs   = 0.026;      % Stator resistance per phase     [Ohm]
-psim = 0.1014;     % Permanent magnet flux linkage   [Wb]
+Pmax = 210e3;      % Maximum power                        [W]
+Tmax = 500;        % Maximum torque                       [N*m]
+Imax = 500;        % Maximum current                      [A]
+Ld   = 140e-6;     % Stator d-axis inductance             [H]
+Lq   = 140e-6;     % Stator q-axis inductance             [H]
+L0   = 0;          % Stator zero-sequence inductance      [H]  not modeled
+Rs   = 9.85e-3;    % Stator resistance per phase          [Ohm]
+psim = 0.06099;    % Permanent magnet flux linkage        [Wb]
 p    = 10;         % Number of pole pairs
-Jm   = 0.0922;     % Rotor inertia                   [Kg*m^2]
+Jm   = 0.05769;    % Rotor inertia                        [Kg*m^2]
+Ke_ds = 0.07823;   % back emf constant in datasheet units [Vrms/RPM]
+Ke   = Ke_ds * sqrt(2); % back emf constant in units we want  [V/RPM]
 
 wmax = inf * pi/30;  % Maximum motor speed [rad/s]
-wbase = 2000 * pi/30; % Base speed [rad/s]
 
 %% High-Voltage Battery Parameters
 Cdc  = 255e-6;     % DC-link capacitor  [F]
-Vnom = 650;        % Nominal DC voltage [V] 
-V1   = 625;        % Voltage V1(< Vnom) [V]
+Vnom = 830;        % Nominal DC voltage [V] 
+V1   = 800;        % Voltage V1(< Vnom) [V]
 
 %% Control Parameters
 Ts   = 5e-6;        % Fundamental sample time               [s]
@@ -30,12 +34,14 @@ Ki_id = 710.3004;   % Integrator gain id controller
 Kp_iq = 1.0744;     % Proportional gain iq controller
 Ki_iq = 1.0615e+03; % Integrator gain iq controller
 
+Mfw_threshold = 0.95;   % Voltage modulation index where field weakening
+                        % begins
+Mfw_saturation = 1.25;  % Voltage modulation index where field weakening
+                        % should saturate
+
+
 %% Zero-Cancellation Transfer Functions
 numd_id = Tsi/(Kp_id/Ki_id);
 dend_id = [1 (Tsi-(Kp_id/Ki_id))/(Kp_id/Ki_id)];
 numd_iq = Tsi/(Kp_iq/Ki_iq);
 dend_iq = [1 (Tsi-(Kp_iq/Ki_iq))/(Kp_iq/Ki_iq)];
-
-%% Current References
-%load pe_ipmsm_35kW_ref_idq;
-%genRefLut
