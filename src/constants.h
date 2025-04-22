8f19bcab61f5217aeed0fa2ddd8677d52164d0b1
Liam Flaherty <liamjfla@gmail.com>
2025-04-22 14:29:30 +1000

    fix constant to correct value and increase precision
    the compiler can reduce to the correct float32 precision
    rather than us doing it manually

--- src/constants.h
+++ src/constants.h
@@ -9,9 +9,9 @@
 #define _CONTROLLIB_CONSTANTS_H_
 
 
-#define ONE_SQRT3  1.732051F  /* 1/sqrt(3) */
-#define TWO_PI     6.283185F  /* 2*pi */
-#define TWO_PI_3   2.094395F  /* 1*pi/3 */
-#define TWO_THIRDS 0.666667F  /* 2/3 */
+#define ONE_SQRT3  0.5773502691896258f  /* 1/sqrt(3) */
+#define TWO_PI     6.2831853071795860f  /* 2*pi */
+#define TWO_PI_3   2.0943951023931953f  /* 2*pi/3 */
+#define TWO_THIRDS 0.6666666666666666f  /* 2/3 */
 
 #endif
